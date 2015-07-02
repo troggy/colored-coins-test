a48320ba114f6bd5b14848a78a30312302a3a76c
Kosta Korenkov <7r0ggy@gmail.com>
2015-07-02 18:02:30 +0300

    Extract metadata using API

--- src/getMetadata.js
+++ src/getMetadata.js
@@ -1,9 +1,47 @@
-var api = require('./coloredApi');
+var api = require('./coloredApi'),
+	wallet = require('../wallet.json');
 
-var assetId = 'LevXAoqMqEk1vwaA4pRwLXVXYycdA79fgr/8ddaf1f77a38d7dc0a9ceb787f217f5d153a9a8eced2dd836ed04944dfcaf076:0';
 
-api.getFrom('assetmetadata', assetId, function(err, body){
+var extractAssets = function(body) {
+	var assets = [];
+	if (body.utxos.length == 0) return assets;
+
+	body.utxos.forEach(function(utxo) {
+		if (utxo.assets.length > 0) {
+			utxo.assets.forEach(function(asset) {
+				assets.push({ assetId: asset.assetId, amount: asset.amount, utxo: utxo.txid + ':' + utxo.index });
+			});
+		}
+	});
+
+	return assets;
+};
+
+var getMetadata = function(assetId, utxo, cb) {
+	api.getFrom('assetmetadata', assetId + "/" + utxo, function(err, body){
+		if (err) {
+			console.log('error: ', err);
+			return;
+		}
+		return cb(body.metadataOfIssuence);
+	});
+};
+
+api.getFrom('addressinfo', wallet.fundedAddress, function(err, body) {
 	if (err) {
 		console.log('error: ', err);
+		return;
 	}
+
+	var assets = extractAssets(body);
+	console.log(assets);
+
+	assets.forEach(function(asset) {
+		getMetadata(asset.assetId, asset.utxo, function(metadata) {
+			metadata.amount = asset.amount;
+			console.log(metadata);
+		});
+
+	});
+
 });
