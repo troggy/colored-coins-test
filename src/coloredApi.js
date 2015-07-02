59ca7aa97e92f8df17ca45654680bab868a3ea20
Kosta Korenkov <7r0ggy@gmail.com>
2015-07-02 18:02:53 +0300

    Use testnet API

--- src/coloredApi.js
+++ src/coloredApi.js
@@ -5,7 +5,7 @@ var request = require('request'),
 module.exports.postTo = function (api_endpoint, json_data, callback) {
 	console.log(api_endpoint + ': ', JSON.stringify(json_data));
 	request.post({
-			url: 'http://api.coloredcoins.org:80/v2/' + api_endpoint,
+			url: 'http://testnet.api.coloredcoins.org:80/v2/' + api_endpoint,
 			headers: {'Content-Type': 'application/json'},
 			form: json_data
 		}, function (error, response, body) {
@@ -16,7 +16,7 @@ module.exports.postTo = function (api_endpoint, json_data, callback) {
 
 module.exports.getFrom = function (api_endpoint, param, callback) {
 	console.log('Get from:' + api_endpoint + '/' + param);
-	request.get('http://api.coloredcoins.org:80/v2/' + api_endpoint + '/' + param, function (error, response, body) {
+	request.get('http://testnet.api.coloredcoins.org:80/v2/' + api_endpoint + '/' + param, function (error, response, body) {
 		apiUtils.handleResponse(error, response, body, callback);
 	});
 };
