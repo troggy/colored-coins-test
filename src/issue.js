277b560ddcc824f8c12c3c3e212cc37b4f4b8387
Kosta Korenkov <7r0ggy@gmail.com>
2015-07-14 14:49:34 +0300

    Reproducing API issues

--- src/issue.js
+++ src/issue.js
@@ -1,9 +1,9 @@
 var bitcoin = require('bitcoinjs-lib'),
-	api = require('./coloredApi'),
-	wallet = require('./../wallet.json');
+    bitcore = require('bitcore'),
+  	api = require('./coloredApi'),
+	  wallet = require('./../wallet.json');
 
-var address = wallet.fundedAddress,
-	key = bitcoin.ECKey.fromWIF(wallet.wif);
+var key = bitcoin.ECKey.fromWIF(wallet.wif);
 
 var signTx = function (unsignedTx, privateKey) {
 	var tx = bitcoin.Transaction.fromHex(unsignedTx);
@@ -15,20 +15,40 @@ var signTx = function (unsignedTx, privateKey) {
 };
 
 var asset = {
-	"issueAddress": address,
+	"issueAddress": "mntLztLU6aKV5nErBSvbf7XaQocaDyTwTf",
 	"amount": 1,
 	"divisibility": 0,
 	"fee": 1000,
 	"transfer": [{
-		"address": address,
+		"address": "2Mv3GRjx3vKLWj2mTDWBmT7mntmwYZW8PkX",
 		"amount": 1
 	}],
-	"metadata": {
-		"issuer": "troggy",
-		"assetName": "Sixth Asset",
+  "financeOutput": {
+    value: "0.920967",
+    n: 2,
+    scriptPubKey: {
+      asm: "OP_DUP OP_HASH160 50d4916a3bbfff5029d93a7bb6a713432aa18e6c OP_EQUALVERIFY OP_CHECKSIG",
+      hex: "76a91450d4916a3bbfff5029d93a7bb6a713432aa18e6c88ac",
+      type: "pubkeyhash"
+    }
+  },
+  "financeOutputTxid": "58dc4938c40f0be24967a42e4cfd7e6df8c21e896160b66660726ad3c8722def",
+  "metadata": {
+    "assetId": "icecream-1",
+		"issuer": "Good Soul, Ltd",
+		"assetName": "Scoop of ice cream",
+    "description": "Generous scoop of vanilla ice cream",
+    "urls": [
+      {
+        "name": "What is ice scream?",
+        "url": "https://en.wikipedia.org/wiki/Ice_cream",
+        "mimeType": "text/html",
+        "dataHash": "2779e4d66d920ca384010faf95b59b18f7940100855b5cd763a14edb980058d3"
+      }
+    ],
 		"userData": {
-			"Name": "Some asset name",
-			"Description": "Very informative description"
+			"flavour": "Vanilla",
+			"size": "100gr"
 		}
 	}
 };
@@ -39,13 +59,17 @@ api.postTo('issue', asset, function (err, body) {
 		return;
 	}
 
-	var signedTxHex = signTx(body.txHex, key);
-	console.log("Signed tx: " + signedTxHex);
+  txHex1 = body.txHex;
 
+  //txHex1 = signTx(txHex1, key);
+  //console.log("Signed tx: " + txHex1);
+
+  console.log(JSON.stringify(new bitcore.Transaction(txHex1).toObject(), null, 2));
+  /*
 	api.postTo('broadcast', { 'txHex': signedTxHex }, function (err, body) {
 		if (err) {
 			console.log('error: ', err);
 			return;
 		}
-	});
+	});*/
 });
--- src/transfer.js
+++ src/transfer.js
@@ -1,9 +1,9 @@
 var bitcoin = require('bitcoinjs-lib'),
+    bitcore = require('bitcore'),
 	api = require('./coloredApi'),
 	wallet = require('./../wallet.json');
 
-var address = wallet.fundedAddress,
-	key = bitcoin.ECKey.fromWIF(wallet.wif);
+var key = bitcoin.ECKey.fromWIF(wallet.wif);
 
 var signTx = function (unsignedTx, privateKey) {
 	var tx = bitcoin.Transaction.fromHex(unsignedTx);
@@ -15,11 +15,21 @@ var signTx = function (unsignedTx, privateKey) {
 };
 
 var asset = {
-  "from": address,
+  "from": "mntLztLU6aKV5nErBSvbf7XaQocaDyTwTf",
   "fee": 1000,
+  "financeOutput": {
+    value: "0.920967",
+    n: 2,
+    scriptPubKey: {
+      asm: "OP_DUP OP_HASH160 50d4916a3bbfff5029d93a7bb6a713432aa18e6c OP_EQUALVERIFY OP_CHECKSIG",
+      hex: "76a91450d4916a3bbfff5029d93a7bb6a713432aa18e6c88ac",
+      type: "pubkeyhash"
+    }
+  },
+  "financeOutputTxid": "58dc4938c40f0be24967a42e4cfd7e6df8c21e896160b66660726ad3c8722def",
   "to": [{
-    "address": "2N5HqAKTaXrfizWN8YdL53EsnsVaDAYyjxK",
-    "amount": 2,
+    "address": "2NBqYzuwPmAyQKEcAMoQM16u8VWR71YMXE1",
+    "amount": 1,
     "assetId": 'LLaaW3fjdiLRPuyzyN9LLRigxRKvYhkwRu'
   }]
 };
@@ -30,13 +40,18 @@ api.postTo('sendasset', asset, function (err, body) {
 		return;
 	}
 
-	var signedTxHex = signTx(body.txHex, key);
-	console.log("Signed tx: " + signedTxHex);
+  txHex1 = body.txHex;
 
-	api.postTo('broadcast', { 'txHex': signedTxHex }, function (err, body) {
+	//txHex1 = signTx(txHex1, key);
+	//console.log("Signed tx: " + txHex1);
+
+
+  console.log(JSON.stringify(new bitcore.Transaction(txHex1).toObject(), null, 2));
+
+	/*api.postTo('broadcast', { 'txHex': signedTxHex }, function (err, body) {
 		if (err) {
 			console.log('error: ', err);
 			return;
 		}
-	});
+	});*/
 });
